5eb2bcc7e0329ac35a2c01c2ab0b23846455b348
CoolEgor61 <vip.Mazda003@bk.ru>
2025-11-23 20:18:24 +0300

    6

--- tasks/petrov_e_find_max_in_columns_matrix/tests/performance/main.cpp
+++ tasks/petrov_e_find_max_in_columns_matrix/tests/performance/main.cpp
@@ -44,18 +44,18 @@ class PetrovERunPerfFindMaxInColumnsMatrix : public ppc::util::BaseRunPerfTests<
   }
 
   bool CheckTestOutputData(OutType &output_data) final {
-    int n = std::get<0>(input_data_);
-    int m = std::get<1>(input_data_);
+    std::size_t n = std::get<0>(input_data_);
+    std::size_t m = std::get<1>(input_data_);
     auto& matrix = std::get<2>(input_data_);
-    int i = 0;
+    std::size_t i = 0;
 
     if (std::cmp_not_equal(m, static_cast<int>(output_data.size()))) {
       return false;
     }
 
     for (i = 0; i < m; i++) {
-      auto start = matrix.begin() + (i * n);
-      auto end = matrix.begin() + n + (i * n);
+      auto start = matrix.begin() + static_cast<std::_Bit_const_iterator::difference_type>(i * n);
+      auto end = matrix.begin() + n + static_cast<std::_Bit_const_iterator::difference_type>(i * n);
       auto element = *std::max_element(start, end);
       if (output_data[i] != element) {
         return false;
