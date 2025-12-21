65934e8accf967f6bf34d17a56246703f484b575
CoolEgor61 <vip.Mazda003@bk.ru>
2025-12-22 02:38:15 +0300

    6

--- tasks/petrov_e_allreduce/mpi/include/ops_mpi.hpp
+++ tasks/petrov_e_allreduce/mpi/include/ops_mpi.hpp
@@ -337,7 +337,7 @@ MPI_Datatype GetMPIDatatype() {
 }
 
 inline int MpiMyAllreduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op,
-                           MPI_Comm comm) {
+                          MPI_Comm comm) {
   int proc_num = 0;
   int proc_rank = 0;
   MPI_Comm_size(comm, &proc_num);
